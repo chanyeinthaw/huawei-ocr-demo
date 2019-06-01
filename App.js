dbd7158be6a8c855fff242379c555c932eef8af5
Chan Nyein Thaw <chanyeinthaw@gmail.com>
2019-06-02 01:53:00 +0630

    Persist token to expires_at date

--- App.js
+++ App.js
@@ -1,30 +1,45 @@
 import React from 'react';
-import {BackHandler, View, StatusBar} from 'react-native'
+import {AsyncStorage, BackHandler, View, StatusBar} from 'react-native'
 import { AppLoading, SplashScreen } from 'expo';
 import CameraPage from "./pages/CameraPage/CameraPage";
 import authenticateRequest from "./request/authenticateRequest";
 
 export default class App extends React.Component {
+    tokenStoreKey = 'xSubjectToken'
+    tokenExpiredAtKey = 'xSubjectTokenExpiresAt'
+
     state = {
         xSubjectToken: ""
     }
 
-    authenticate = () => {
-        authenticateRequest().then((response) => {
-            if (response.ok == true) {
-                this.setState({
-                    xSubjectToken: response.headers.get('X-Subject-Token')
-                })
+    authenticate = async () => {
+        const token = await AsyncStorage.getItem(this.tokenStoreKey, () => {})
+        const expiresAt = await AsyncStorage.getItem(this.tokenExpiredAtKey, () => {})
 
+        if (token != null && expiresAt != null && expiresAt < new Date()) {
+            this.setState({xSubjectToken: token})
+            SplashScreen.hide()
+
+            return
+        }
+
+        authenticateRequest().then(async (response) => {
+            if (response.ok == true) {
+                const token = response.headers.get('X-Subject-Token')
+                const data = await response.json()
+                const _token = data.token || {}
+
+                await AsyncStorage.setItem(this.tokenStoreKey, token)
+                await AsyncStorage.setItem(this.tokenExpiredAtKey, new Date(_token.expires_at || Date.now()))
+
+                this.setState({xSubjectToken: token})
                 SplashScreen.hide()
 
                 return
             }
 
             BackHandler.exitApp()
-        }).catch((e) => {
-            BackHandler.exitApp()
-        })
+        }).catch(() => {BackHandler.exitApp()})
     }
 
     render() {
